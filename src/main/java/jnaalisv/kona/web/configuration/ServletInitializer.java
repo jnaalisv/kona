6d065f5e479c9b0a26318d44b01c60328d3974ce
Juho Naalisvaara <juho.naalisvaara@trimico.fi>
2016-01-09 17:19:10 +0200

    simple rest interface with spring-webmvc

---
+++ src/main/java/jnaalisv/kona/web/configuration/ServletInitializer.java
@@ -0,0 +1,27 @@
+package jnaalisv.kona.web.configuration;
+
+import org.springframework.web.servlet.support.AbstractAnnotationConfigDispatcherServletInitializer;
+
+public class ServletInitializer extends AbstractAnnotationConfigDispatcherServletInitializer {
+
+    @Override
+    protected Class<?>[] getRootConfigClasses() {
+        return new Class[] { };
+    }
+
+    @Override
+    protected Class<?>[] getServletConfigClasses() {
+        return new Class[] { WebConfiguration.class};
+    }
+
+    @Override
+    protected String[] getServletMappings() {
+        return new String[] { "/" };
+    }
+
+    @Override
+    protected String getServletName() {
+        return "Kona";
+    }
+}
+
