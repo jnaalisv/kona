c8cbd408594c632bc6581536a02ab29f03748e2f
Juho Naalisvaara <juho.naalisvaara@trimico.fi>
2016-01-10 21:54:48 +0200

    refactored duplicate code in test classes

---
+++ src/integrationTest/java/jnaalisv/kona/web/interfaces/AbstractSpringRestMvcTest.java
@@ -0,0 +1,36 @@
+package jnaalisv.kona.web.interfaces;
+
+import javax.inject.Inject;
+
+import org.junit.Before;
+import org.junit.runner.RunWith;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.context.web.WebAppConfiguration;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.web.context.WebApplicationContext;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+import jnaalisv.kona.web.configuration.WebConfiguration;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@WebAppConfiguration
+@ContextConfiguration(classes = {WebConfiguration.class })
+public abstract class AbstractSpringRestMvcTest {
+
+    @Inject
+    private WebApplicationContext webApplicationContext;
+
+    protected final ObjectMapper objectMapper = new ObjectMapper();
+
+    protected MockMvc mockMvc;
+
+    @Before
+    public void initMockMvc() {
+        mockMvc = MockMvcBuilders
+                .webAppContextSetup(webApplicationContext)
+                .build();
+    }
+}
--- src/integrationTest/java/jnaalisv/kona/web/interfaces/addresses/AddressControllerIntegrationTest.java
+++ src/integrationTest/java/jnaalisv/kona/web/interfaces/addresses/AddressControllerIntegrationTest.java
@@ -5,42 +5,18 @@ import static org.springframework.test.web.servlet.request.MockMvcRequestBuilder
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.header;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
-import javax.inject.Inject;
-
 import org.junit.Test;
-import org.junit.runner.RunWith;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
-import org.springframework.test.context.ContextConfiguration;
-import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
-import org.springframework.test.context.web.WebAppConfiguration;
-import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.MvcResult;
-import org.springframework.test.web.servlet.setup.MockMvcBuilders;
-import org.springframework.web.context.WebApplicationContext;
-
-import com.fasterxml.jackson.databind.ObjectMapper;
 
 import jnaalisv.kona.domain.addresses.AddressDTO;
-import jnaalisv.kona.web.configuration.WebConfiguration;
+import jnaalisv.kona.web.interfaces.AbstractSpringRestMvcTest;
 
-@RunWith(SpringJUnit4ClassRunner.class)
-@WebAppConfiguration
-@ContextConfiguration(classes = {WebConfiguration.class })
-public class AddressControllerIntegrationTest {
-
-    @Inject
-    private WebApplicationContext webApplicationContext;
-
-    private final ObjectMapper objectMapper = new ObjectMapper();
+public class AddressControllerIntegrationTest extends AbstractSpringRestMvcTest {
 
     @Test
     public void shouldSuccesfullyCreateNewAddress() throws Exception {
-
-        MockMvc mockMvc = MockMvcBuilders
-                .webAppContextSetup(webApplicationContext)
-                .build();
-
         AddressDTO aNewAddress = new AddressDTO();
         aNewAddress.street = "Street Address";
         aNewAddress.postalCode = "99999";
