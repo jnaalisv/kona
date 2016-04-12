1b6aa76e2c4349675d500d130eb54cde15a7c9b5
Juho Naalisvaara <juho.naalisvaara@trimico.fi>
2016-04-12 22:52:36 +0300

    added customers rest interface

---
+++ src/main/java/kona/web/customer/CustomerController.java
@@ -0,0 +1,43 @@
+package kona.web.customer;
+
+import java.util.Collections;
+import java.util.List;
+
+import javax.inject.Inject;
+
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("customers")
+public class CustomerController {
+
+    @Inject
+    public CustomerController() {
+    }
+
+    @RequestMapping(method = RequestMethod.GET)
+    public List<CustomerDTO> getAll() {
+        return Collections.emptyList();
+    }
+
+    @RequestMapping(path = "{id}", method = RequestMethod.GET)
+    public CustomerDTO get(@PathVariable long id) {
+        return new CustomerDTO();
+    }
+
+    @RequestMapping(method = RequestMethod.POST)
+    public ResponseEntity<CustomerDTO> post(@RequestBody CustomerDTO addressDTO) {
+
+        HttpHeaders responseHeaders = new HttpHeaders();
+        responseHeaders.set("Location", "customers/");
+        return new ResponseEntity<>(new CustomerDTO(), responseHeaders, HttpStatus.CREATED);
+    }
+
+}
---
+++ src/main/java/kona/web/customer/CustomerDTO.java
@@ -0,0 +1,7 @@
+package kona.web.customer;
+
+public class CustomerDTO {
+
+    public Long id;
+    public String name;
+}
