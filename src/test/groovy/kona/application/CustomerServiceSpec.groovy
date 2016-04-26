31c59ca918396ec66230aee46788176b6cc87739
Juho Naalisvaara <juho.naalisvaara@phz.fi>
2016-04-26 08:44:10 +0300

    added another spec

--- src/test/groovy/kona/application/CustomerServiceSpec.groovy
+++ src/test/groovy/kona/application/CustomerServiceSpec.groovy
@@ -5,15 +5,29 @@ import spock.lang.Specification
 
 class CustomerServiceSpec extends Specification {
 
-    def "loadAll should call getAll" () {
+    def customerRepositoryMock = Mock(CustomerRepository)
+    def customerService = new CustomerService(customerRepositoryMock);
 
-        def customerRepositoryMock = Mock(CustomerRepository)
-        def customerService = new CustomerService(customerRepositoryMock);
+    def "loadAll should call getAll" () {
 
         when:
         customerService.loadAll()
 
         then:
         1 * customerRepositoryMock.getAll()
+        0 * _
+    }
+
+    def "load should call get with id" () {
+
+        given:
+        long customerID = 124l
+
+        when:
+        customerService.load(customerID)
+
+        then:
+        1 * customerRepositoryMock.get(customerID)
+        0 * _
     }
 }
