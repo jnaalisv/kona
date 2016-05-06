3b909e9944679e67413ac7231a6d0fd4145cf4a0
Juho Naalisvaara <juho.naalisvaara@phz.fi>
2016-05-06 16:23:01 +0300

    added integration test for repo

---
+++ src/integrationTest/java/kona/infrastructure/AddressRepositoryIntegrationTest.java
@@ -0,0 +1,58 @@
+package kona.infrastructure;
+
+import kona.infrastructure.config.PersistenceConfiguration;
+import kona.model.domain.address.Address;
+import kona.model.domain.address.AddressRepository;
+import org.junit.Test;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.AbstractTransactionalJUnit4SpringContextTests;
+
+import javax.inject.Inject;
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@ContextConfiguration(classes = {PersistenceConfiguration.class})
+public class AddressRepositoryIntegrationTest extends AbstractTransactionalJUnit4SpringContextTests {
+
+    @PersistenceContext
+    private EntityManager entityManager;
+
+    @Inject
+    private AddressRepository addressRepository;
+
+    @Test
+    public void shouldAddAddress() {
+        assertThat(countRowsInTable("address")).isEqualTo(0);
+
+        Address mannerheimintieYksi = new Address("Mannerheimintie 1", "00100", "Helsinki");
+
+        addressRepository.add(mannerheimintieYksi);
+
+        entityManager.flush();
+
+        assertThat(mannerheimintieYksi.getID()).isGreaterThan(0);
+
+        assertThat(countRowsInTableWhere("address", "street='Heikinkatu 1'")).isEqualTo(0);
+        assertThat(countRowsInTableWhere("address", "street='Mannerheimintie 1' and postalCode = '00100' and municipality='Helsinki'")).isEqualTo(1);
+    }
+
+    @Test
+    public void shouldGetAddress() {
+        assertThat(countRowsInTable("address")).isEqualTo(0);
+
+        Address mannerheimintieYksi = new Address("Mannerheimintie 1", "00100", "Helsinki");
+
+        addressRepository.add(mannerheimintieYksi);
+
+        entityManager.flush();
+
+        Address addressFromRepository = addressRepository
+                .get(mannerheimintieYksi.getID())
+                .orElseThrow(() -> new RuntimeException("test failure"));
+
+        assertThat(addressFromRepository.getMunicipality()).isEqualTo(mannerheimintieYksi.getMunicipality());
+    }
+
+}
