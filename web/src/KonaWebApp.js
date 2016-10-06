ba169f02a726825affeafed47b9d23584191ade0
Juho Naalisvaara <juho.naalisvaara@phz.fi>
2016-10-06 16:35:24 +0300

    add simple login form

--- web/src/KonaWebApp.js
+++ web/src/KonaWebApp.js
@@ -1,11 +1,25 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
 
 class KonaWebApp extends Component {
+
+    login(event) {
+        event.preventDefault();
+        console.log('this.username='+ this.username.value);
+        console.log('this.password='+ this.password.value);
+        if (this.username.value && this.password.value) {
+
+        }
+    }
+
     render() {
         return (
-            <p>Hello, World!</p>
+            <div>
+                <form onSubmit={(e) => this.login(e)} >
+                    username: <input required type="text" ref={(input) => this.username = input} placeholder="username"/>
+                    password: <input required type="password" ref={(input) => this.password = input} placeholder="password"/>
+                    <button type="submit">login</button>
+                </form>
+            </div>
         );
     }
 }
