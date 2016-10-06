dede612a6ae6acfb6d2bdc5871e32a957ace23bb
Juho Naalisvaara <juho.naalisvaara@phz.fi>
2016-10-06 14:24:42 +0300

    rename scaffolded React application

---
+++ web/src/KonaWebApp.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+class KonaWebApp extends Component {
+    render() {
+    return (
+        <p>Hello, World!</p>
+    );
+    }
+}
+
+export default KonaWebApp;
--- web/src/KonaWebApp.test.js
+++ web/src/KonaWebApp.test.js
@@ -1,8 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './App';
+import KonaWebApp from './KonaWebApp';
 
 it('renders without crashing', () => {
   const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
+  ReactDOM.render(<KonaWebApp />, div);
 });
