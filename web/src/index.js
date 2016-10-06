05776fc27542d08da4d51aba01aa67f3d1a44372
Juho Naalisvaara <juho.naalisvaara@phz.fi>
2016-10-06 18:49:23 +0300

    use react-router

--- web/src/index.js
+++ web/src/index.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { render } from 'react-dom';
+import { BrowserRouter, Match, Miss } from 'react-router';
 import KonaWebApp from './components/KonaWebApp';
 
 render(<KonaWebApp />, document.getElementById('root'));
