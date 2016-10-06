4bb3a983d7e31c46122f677753a3c9df644408b6
Juho Naalisvaara <juho.naalisvaara@phz.fi>
2016-10-06 17:48:19 +0300

    enable CORS

--- src/main/java/kona/web/config/WebConfiguration.java
+++ src/main/java/kona/web/config/WebConfiguration.java
@@ -1,15 +1,16 @@
 package kona.web.config;
 
-import java.util.List;
-
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.converter.HttpMessageConverter;
 import org.springframework.http.converter.StringHttpMessageConverter;
 import org.springframework.http.converter.json.Jackson2ObjectMapperBuilder;
 import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+
+import java.util.List;
 
 /**
  *
@@ -32,6 +33,11 @@ public class WebConfiguration extends WebMvcConfigurerAdapter {
         jackson2ObjectMapperBuilder.indentOutput(true);
         converters.add(new MappingJackson2HttpMessageConverter(jackson2ObjectMapperBuilder.build()));
         converters.add(new StringHttpMessageConverter());
+    }
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**");
     }
 }
 
