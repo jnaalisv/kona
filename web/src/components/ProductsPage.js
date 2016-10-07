81bc3bd6cf67f80c2550827498e9ee8c9fd2a276
Juho Naalisvaara <juho.naalisvaara@phz.fi>
2016-10-07 18:26:52 +0300

    add ProducstPage

---
+++ web/src/components/ProductsPage.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+class ProductsPage extends React.Component {
+    render() {
+        return (
+            <div>
+                <h3>products</h3>
+            </div>
+        )
+    }
+}
+
+export default ProductsPage;
--- web/src/index.js
+++ web/src/index.js
@@ -3,12 +3,14 @@ import { render } from 'react-dom';
 import { BrowserRouter, Match, Miss } from 'react-router';
 import NotFound from './components/NotFound';
 import LoginPage from './components/LoginPage';
+import ProductsPage from './components/ProductsPage'
 
 const Root = () => {
     return (
         <BrowserRouter>
             <div>
                 <Match exactly pattern="/login" component={LoginPage} />
+                <Match exactly pattern="/products" component={ProductsPage} />
                 <Miss component={NotFound} />
             </div>
         </BrowserRouter>
