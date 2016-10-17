99695d42ce4469932a22741c0200bf01b3c862f4
Juho Naalisvaara <juho.naalisvaara@phz.fi>
2016-10-17 15:24:38 +0300

    convert to enzyme shallow rendering test

--- web/src/components/LoginPage.test.js
+++ web/src/components/LoginPage.test.js
@@ -1,8 +1,10 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import {shallow} from 'enzyme';
 import LoginPage from './LoginPage';
 
-it('renders without crashing', () => {
-  // const div = document.createElement('div');
-  // ReactDOM.render(<LoginPage />, div);
+it('LoginPage calls login after click', () => {
+
+    const loginMock = jest.fn();
+
+    const loginPage = shallow(<LoginPage login={loginMock} />);
 });
