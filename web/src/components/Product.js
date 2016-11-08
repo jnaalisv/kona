add87f13ea341593d02c22a72ed2b9eae1263bf9
Juho Naalisvaara <juho.naalisvaara@trimico.fi>
2016-11-08 22:13:49 +0200

    fetch and display one product

--- web/src/components/Product.js
+++ web/src/components/Product.js
@@ -1,9 +1,47 @@
 import React from 'react'
 
+const url = 'http://localhost:9999/kona/products/';
+
+const httpInit = {
+    method: 'GET',
+    mode: 'cors',
+    headers: {
+        'ContentType': 'application/json'
+    }
+};
+
 class Product extends React.Component {
+    constructor() {
+        super();
+        this.state = {product: {}};
+
+        this.getProduct = this.getProduct.bind(this);
+    }
+
+    getProduct(productId) {
+        return window
+            .fetch(url + productId, httpInit)
+            .then((response) => {
+                return response.json();
+            }).catch((error) => {
+                console.log('error: ' + error);
+                return null;
+            });
+    }
+
+    componentDidMount() {
+        this.getProduct(this.props.params.productId)
+            .then((product => {
+                this.setState({product: product});
+            }));
+    }
+
     render () {
         return (
-            <p>de product</p>
+            <div>
+                <div>name: {this.state.product.name}</div>
+                <div>productCode: {this.state.product.productCode}</div>
+            </div>
         )
     }
 }
