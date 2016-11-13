b15d5cfa83e17690b28a4642cbe99c453c785ac0
Juho Naalisvaara <juho.naalisvaara@trimico.fi>
2016-11-13 17:48:21 +0200

    refactor routing logic to separate component

---
+++ web/src/components/products/ProductTable.js
@@ -0,0 +1,43 @@
+import React from 'react'
+
+import ProductRow from './ProductRow'
+import productService from './ProductService'
+
+class ProductTable extends React.Component {
+    constructor() {
+        super();
+        this.state = {
+            products: []
+        }
+    };
+
+    componentDidMount() {
+        productService.getProducts()
+            .then((products => {
+                const productRows = products.map((product, index) => <ProductRow key={index} product={product}/>);
+                this.setState({products: productRows});
+            }))
+            .catch((error) => {
+                console.log('error ' + error);
+                return [];
+            })
+    }
+
+    render() {
+        return (
+            <table>
+                <thead>
+                <tr>
+                    <th>code</th>
+                    <th>name</th>
+                </tr>
+                </thead>
+                <tbody>
+                {this.state.products}
+                </tbody>
+            </table>
+        )
+    }
+}
+
+export default ProductTable;
--- web/src/components/products/ProductsView.js
+++ web/src/components/products/ProductsView.js
@@ -1,52 +1,16 @@
 import React from 'react'
 import {Match} from 'react-router'
 
-import ProductRow from './ProductRow'
+import ProductTable from './ProductTable'
 import Product from './Product'
-import productService from './ProductService'
 
 class ProductsView extends React.Component {
-    constructor() {
-        super();
-        this.state = {
-            products: [],
-            lastError: null
-        }
-    };
-
-    componentDidMount() {
-        productService.getProducts()
-            .then((products => {
-                const productRows = products.map((product, index) => <ProductRow key={index} product={product}/>);
-                this.setState({products: productRows});
-            }))
-            .catch((error) => {
-                this.setState({lastError: error.message});
-                return [];
-            })
-    }
 
     render() {
-
-        const lastError = this.state.lastError;
-
         return (
             <div>
                 <Match pattern={`${this.props.pathname}/:productId`} component={Product}/>
-                <Match pattern={this.props.pathname} exactly render={() => (
-                    <table>
-                        <thead>
-                        <tr>
-                            <th>code</th>
-                            <th>name</th>
-                        </tr>
-                        </thead>
-                        <tbody>
-                        {this.state.products}
-                        </tbody>
-                    </table>
-                )}/>
-                {lastError && <p >{lastError}</p>}
+                <Match pattern={this.props.pathname} exactly component={ProductTable}/>
             </div>
         )
     }
