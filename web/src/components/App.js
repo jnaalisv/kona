e37f671c0ca4442031de1ce9cc4fe93c818baf48
Juho Naalisvaara <juho.naalisvaara@trimico.fi>
2016-11-27 11:42:35 +0200

    remove stuff

--- web/src/components/App.js
+++ web/src/components/App.js
@@ -16,43 +16,6 @@ const NoMatch = () => (
     </div>
 );
 
-const Topic = (props) => (
-    <div>
-        <h3>{props.params.topicId}</h3>
-        <p>location.pathname: {location.pathname}</p>
-        <p>params.topicId: {props.params.topicId}</p>
-        <p>pathname: {props.pathname}</p>
-        <p>pattern: {props.pattern}</p>
-    </div>
-);
-
-const Topics = ({ pathname, pattern }) => (
-    // 5. Components rendered by a `Match` get some routing-specific
-    //    props, like the portion of the parent `pattern` that was
-    //    matched against the current `location.pathname`, in this case
-    //    `/topics`
-    <div>
-        <h2>Topics</h2>
-        <ul>
-            {/* 6. Use the parent's matched pathname to link relatively */}
-            <li><Link to={`${pathname}/rendering`}>Rendering with React</Link></li>
-            <li><Link to={`${pathname}/components`}>Components</Link></li>
-            <li><Link to={`${pathname}/props-v-state`}>Props v. State</Link></li>
-        </ul>
-
-        {/* 7. Render more `Match` components to get nesting naturally
-         within the render lifecycle. Use the parent's matched
-         pathname to nest the url.
-         */}
-        <Match pattern={`${pathname}/:topicId`} component={Topic}/>
-
-        {/* 8. use the `render` prop for convenient inline rendering */}
-        <Match pattern={pathname} exactly render={() => (
-            <h3>Please select a topic</h3>
-        )}/>
-    </div>
-);
-
 class App extends React.Component {
 
     render() {
@@ -67,14 +30,12 @@ class App extends React.Component {
                     <nav>
                         <ul>
                             <li><Link to="/">Home</Link></li>
-                            <li><Link to="/topics">Topics</Link></li>
                             <li><Link to="/products">Products</Link></li>
                         </ul>
                     </nav>
 
                     <div id="content">
                         <Match exactly pattern="/" component={Home} />
-                        <Match pattern="/topics" component={Topics} />
                         <Match pattern="/products" component={ProductsView} />
                         <Miss component={NoMatch}/>
                     </div>
