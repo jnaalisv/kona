d80a5d641ae5bc0debe99a97fa461f663d9154fb
Juho Naalisvaara <juho.naalisvaara@trimico.fi>
2016-11-27 12:44:37 +0200

    add jackson support for java8 types and param names

--- server/src/main/java/kona/web/config/WebConfiguration.java
+++ server/src/main/java/kona/web/config/WebConfiguration.java
@@ -3,6 +3,7 @@ package kona.web.config;
 import com.fasterxml.jackson.databind.DeserializationFeature;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.SerializationFeature;
+import com.fasterxml.jackson.datatype.jdk8.Jdk8Module;
 import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
@@ -34,6 +35,7 @@ public class WebConfiguration extends WebMvcConfigurerAdapter {
     private ObjectMapper objectMapper() {
         ObjectMapper objectMapper = new ObjectMapper();
         objectMapper.registerModule(new JavaTimeModule());
+        objectMapper.registerModule(new Jdk8Module());
         objectMapper.configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, true);
         objectMapper.configure(SerializationFeature.INDENT_OUTPUT, true);
         objectMapper.configure(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS, false);
