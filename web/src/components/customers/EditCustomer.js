ea1c9a8c9115df0aecaa39f8784d99931b1e8815
Juho Naalisvaara <juho.naalisvaara@trimico.fi>
2016-12-14 23:31:49 +0200

    add ui for editing customer addresses

--- web/src/components/customers/EditCustomer.js
+++ web/src/components/customers/EditCustomer.js
@@ -6,11 +6,13 @@ import HttpError from '../HttpError'
 class EditCustomer extends React.Component {
     constructor() {
         super();
-        this.state = {customer: {name:''},notifications: []};
+        this.state = {customer: {name:'', addresses:[]},notifications: []};
 
         this.addError = this.addError.bind(this);
         this.onChange = this.onChange.bind(this);
         this.saveCustomer = this.saveCustomer.bind(this);
+        this.addAddress = this.addAddress.bind(this);
+        this.onAddressChange = this.onAddressChange.bind(this);
     }
 
     addError(error) {
@@ -47,6 +49,12 @@ class EditCustomer extends React.Component {
             }, this.addError);
     }
 
+    addAddress(e) {
+        const customer = {...this.state.customer};
+        customer.addresses.push({});
+        this.setState({customer});
+    }
+
     onChange(event) {
         event.preventDefault();
         const customer = {...this.state.customer};
@@ -54,9 +62,18 @@ class EditCustomer extends React.Component {
         this.setState({customer});
     }
 
+    onAddressChange(event, index) {
+        event.preventDefault();
+
+        const customer = {...this.state.customer};
+
+        customer.addresses[index][event.target.name] = event.target.value;
+
+        this.setState({customer});
+    }
+
     render () {
         const customer = this.state.customer;
-
         return (
             <div>
                 <div>id: {customer.id}</div>
@@ -64,6 +81,32 @@ class EditCustomer extends React.Component {
                 <div>created: {customer.createTime}</div>
 
                 <div>name: <input name="name" value={customer.name} onChange={this.onChange}/></div>
+
+                <table>
+                    <tr>
+                        <th>street</th>
+                        <th>postal code</th>
+                        <th>municipality</th>
+                        <th>country code</th>
+                    </tr>
+
+                    {Object.keys(customer.addresses)
+                        .map(index => {
+                            return (
+                                <tr key={index}>
+                                    <td><input name="street" value={this.state.customer.addresses[index].street} onChange={(e) => this.onAddressChange(e, index)}/></td>
+                                    <td><input name="postalCode" value={this.state.customer.addresses[index].postalCode} onChange={(e) => this.onAddressChange(e, index)}/></td>
+                                    <td><input name="municipality" value={this.state.customer.addresses[index].municipality} onChange={(e) => this.onAddressChange(e, index)}/></td>
+                                    <td><input name="countryCode" value={this.state.customer.addresses[index].countryCode} onChange={(e) => this.onAddressChange(e, index)}/></td>
+                                </tr>
+                            )
+                        })
+                    }
+
+                </table>
+
+                <button onClick={(e) => this.addAddress(e)}>Add Address</button>
+
                 <button onClick={(e) => this.saveCustomer(e)}>Save</button>
                 <Notifications notifications={this.state.notifications} />
             </div>
