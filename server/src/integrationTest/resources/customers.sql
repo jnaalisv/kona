570c38c0374f60e21a54689b95d6d28d6c606fce
Juho Naalisvaara <juho.naalisvaara@trimico.fi>
2016-12-29 22:54:10 +0200

    fix test data

--- server/src/integrationTest/resources/customers.sql
+++ server/src/integrationTest/resources/customers.sql
@@ -1,5 +1,5 @@
 insert
-into customer(id, name)
+into customer(id, name, version)
 values
-  ((select nextval('hibernate_sequence')), 'Some Customer'),
-  ((select nextval('hibernate_sequence')), 'Another Customer');
+  ((select nextval('hibernate_sequence')), 'Some Customer', 0),
+  ((select nextval('hibernate_sequence')), 'Another Customer', 0);
