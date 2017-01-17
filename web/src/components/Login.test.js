7f8e74c98e27112f027bcef814ea2c780c2acf02
Juho Naalisvaara <juho.naalisvaara@trimico.fi>
2017-01-17 18:47:33 +0200

    remove old login stuff

--- web/src/components/Login.test.js
+++
@@ -1,8 +0,0 @@
-import React from 'react'
-import {render } from 'react-dom'
-import Login from './Login'
-
-test('Login renders without crashing', () => {
-    const div = document.createElement('div');
-    render(<Login />, div);
-});
