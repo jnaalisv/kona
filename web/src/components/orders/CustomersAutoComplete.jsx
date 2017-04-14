bbfb9940365b86b6f37f6b956c732282390ced53
Juho Naalisvaara <juho.naalisvaara@phz.fi>
2017-04-15 00:00:32 +0300

    improvments

--- web/src/components/orders/CustomersAutoComplete.jsx
+++ web/src/components/orders/CustomersAutoComplete.jsx
@@ -5,7 +5,7 @@ class CustomersAutoComplete extends React.Component {
     constructor() {
         super();
 
-        this.state = {query: '', results:[]};
+        this.state = {query: '', results:[], selected: undefined};
 
         this.onChange = this.onChange.bind(this);
     }
@@ -13,11 +13,17 @@ class CustomersAutoComplete extends React.Component {
     onChange(event) {
         event.preventDefault();
         const newQuery = event.target.value;
+
         this.setState({query: newQuery, results: []});
 
         if (newQuery && newQuery.length > 1) {
             getCustomers(newQuery).then(response => this.setState({results: response}));
         }
+    }
+
+    select(index) {
+        const newSelection = {...this.state.results[index]};
+        this.setState({query: newSelection.name, results: [], selected: newSelection});
     }
 
     render() {
@@ -28,7 +34,14 @@ class CustomersAutoComplete extends React.Component {
                 {
                     Object
                         .keys(this.state.results)
-                        .map(index => <div className={index === 0 ? 'autocomplete selected' : 'autocomplete'} key={index}>{this.state.results[index].id}: {this.state.results[index].name}</div>)
+                        .map(index =>
+                            <div
+                                className={index === 0 ? 'autocomplete selected' : 'autocomplete'}
+                                key={index}
+                                onClick={() => this.select(index)}>
+                                {this.state.results[index].id}: {this.state.results[index].name}
+                            </div>
+                        )
                 }
             </span>
         )
