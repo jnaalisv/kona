5ce7a8fa2e01bc9aaf9cc8e3169d6dfeac673178
Juho Naalisvaara <juho.naalisvaara@gmail.com>
2018-04-11 09:24:15 +0300

    remove unused imports

--- server/src/main/java/kona/web/config/WebConfiguration.java
+++ server/src/main/java/kona/web/config/WebConfiguration.java
@@ -1,17 +1,5 @@
 package kona.web.config;
 
-import com.fasterxml.jackson.databind.ObjectMapper;
-import org.springframework.context.annotation.ComponentScan;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.Import;
-import org.springframework.http.converter.HttpMessageConverter;
-import org.springframework.http.converter.StringHttpMessageConverter;
-import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter;
-import org.springframework.web.servlet.config.annotation.EnableWebMvc;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
-
-import java.util.List;
-
 /**
  *
  * @EnableWebMvc annotation does a number of useful things – specifically, in
