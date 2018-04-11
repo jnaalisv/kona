c10bb3dd35696f4acee391479569904478a8c55a
Juho Naalisvaara <juho.naalisvaara@gmail.com>
2018-04-11 15:02:24 +0300

    restore test ds

--- server/src/integrationTest/java/kona/IntegrationTestConfig.java
+++ server/src/integrationTest/java/kona/IntegrationTestConfig.java
@@ -9,14 +9,14 @@ import java.util.Properties;
 
 public class IntegrationTestConfig {
 
-//    @Bean(destroyMethod = "close")
-//    public DataSource actualDataSource() {
-//        Properties props = new Properties();
-//        props.setProperty("dataSourceClassName", "org.postgresql.ds.PGSimpleDataSource");
-//        props.setProperty("dataSource.user", "kona_test");
-//        props.setProperty("dataSource.password", "kona_test");
-//        props.setProperty("dataSource.databaseName", "kona_test");
-//        HikariConfig dataSourceConfig = new HikariConfig(props);
-//        return new HikariDataSource(dataSourceConfig);
-//    }
+    @Bean(destroyMethod = "close")
+    public DataSource actualDataSource() {
+        Properties props = new Properties();
+        props.setProperty("dataSourceClassName", "org.postgresql.ds.PGSimpleDataSource");
+        props.setProperty("dataSource.user", "kona_test");
+        props.setProperty("dataSource.password", "kona_test");
+        props.setProperty("dataSource.databaseName", "kona_test");
+        HikariConfig dataSourceConfig = new HikariConfig(props);
+        return new HikariDataSource(dataSourceConfig);
+    }
 }
