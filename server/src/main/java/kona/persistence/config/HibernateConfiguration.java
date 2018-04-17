f54fdb7b003ba12ac8114e44325e90be74e4b101
Juho Naalisvaara <juho.naalisvaara@gmail.com>
2018-04-17 10:41:45 +0300

    build Hibernate SessionFactory with SessionFactoryBuilder and use properties from application.yml

--- server/src/main/java/kona/persistence/config/HibernateConfiguration.java
+++ server/src/main/java/kona/persistence/config/HibernateConfiguration.java
@@ -1,52 +1,34 @@
 package kona.persistence.config;
 
 import org.hibernate.SessionFactory;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.Import;
 import org.springframework.orm.hibernate5.HibernateTransactionManager;
-import org.springframework.orm.hibernate5.LocalSessionFactoryBean;
+import org.springframework.orm.hibernate5.LocalSessionFactoryBuilder;
 
 import javax.sql.DataSource;
-import java.beans.PropertyVetoException;
 import java.util.Properties;
 
 @Configuration
+@EnableConfigurationProperties
 @ComponentScan(basePackages = {"kona.persistence.impl"})
 public class HibernateConfiguration {
 
-    private static Properties hibernateProperties() {
-        Properties hibernateProperties = new Properties();
-
-        hibernateProperties.put("hibernate.dialect", "org.hibernate.dialect.PostgreSQLDialect");
-
-        //Specifies the action that is invoked to the database when the Hibernate SessionFactory is created or closed.
-        hibernateProperties.put("hibernate.hbm2ddl.auto","validate");
-
-        //Configures the naming strategy that is used when Hibernate creates
-        //new database objects and schema elements
-        hibernateProperties.put("hibernate.ejb.naming_strategy", "org.hibernate.cfg.ImprovedNamingStrategy");
-
-        hibernateProperties.put("hibernate.format_sql", "true");
-        hibernateProperties.put("hibernate.generate_statistics", "true");
-
-        // configure batching
-        hibernateProperties.put("hibernate.jdbc.batch_size", 30);
-        hibernateProperties.put("hibernate.order_updates", "true");
-        hibernateProperties.put("hibernate.order_inserts", "true");
-        hibernateProperties.put("hibernate.jdbc.batch_versioned_data", "true");
-
-        return hibernateProperties;
+    @Bean
+    @ConfigurationProperties("spring.jpa.properties")
+    public Properties hibernateProperties() {
+        return new Properties();
     }
 
     @Bean
-    public LocalSessionFactoryBean sessionFactory(DataSource dataSource) throws PropertyVetoException {
-        LocalSessionFactoryBean sessionFactory = new LocalSessionFactoryBean();
-        sessionFactory.setDataSource(dataSource);
-        sessionFactory.setPackagesToScan("kona.domain");
-        sessionFactory.setHibernateProperties(hibernateProperties());
-        return sessionFactory;
+    public SessionFactory sessionFactory(DataSource dataSource, Properties hibernateProperties) {
+        return new LocalSessionFactoryBuilder(dataSource)
+                .scanPackages("kona.domain")
+                .addProperties(hibernateProperties)
+                .buildSessionFactory();
     }
 
     @Bean
