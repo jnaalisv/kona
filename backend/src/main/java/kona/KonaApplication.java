56d491106a2a28a0d8e26ec9827bab38c4d86843
Juho Naalisvaara <juho.naalisvaara@gmail.com>
2018-10-13 15:02:37 +0300

    web app with Spring Boot 2.1.0.M4

---
+++ backend/src/main/java/kona/KonaApplication.java
@@ -0,0 +1,12 @@
+package kona;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KonaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KonaApplication.class, args);
+    }
+}
