1f9df1621e168eab2d887f1579ad82d71b1bbdd1
Juho Naalisvaara <juho.naalisvaara@gmail.com>
2018-10-13 23:40:47 +0300

    remove old stuff

--- server/src/integrationTest/resources/products.sql
+++
@@ -1,7 +0,0 @@
-insert
-into product(id, version, name, productCode, productType)
-values((select nextval('hibernate_sequence')), 0, 'Typewriter, Mk. I.', 'TW.I', 'EQUIPMENT')
-  , ((select nextval('hibernate_sequence')), 0, 'Central Computing Unit', 'CCU', 'EQUIPMENT')
-  , ((select nextval('hibernate_sequence')), 0, 'Liquid Crystal Display', 'LCD', 'EQUIPMENT')
-  , ((select nextval('hibernate_sequence')), 0, 'Key Based Input Device, Mk. I', 'KBID.I', 'EQUIPMENT')
-  , ((select nextval('hibernate_sequence')), 0, 'Portable Touch-Screen Device, Prototype', 'PTSD', 'EQUIPMENT');
