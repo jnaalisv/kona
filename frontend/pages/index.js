428519ecf1d500b2de64d1d31fe3b583ae0f019e
Juho Naalisvaara <juho.naalisvaara@gmail.com>
2018-10-14 22:51:19 +0300

    simple fetch and show data

--- frontend/pages/index.js
+++ frontend/pages/index.js
@@ -1,1 +1,25 @@
-export default () => <div>Hello, world!</div>
+import fetch from 'isomorphic-unfetch'
+import Link from 'next/link'
+
+const Page = ({ products }) => {
+    return (
+    <div>
+        <ul>
+            {products.map(product => (
+                <li key={product.id}>
+                    <Link as={`/products/${product.id}`} href={`/products/${product.id}`}>
+                    <a>{product.name}</a>
+                    </Link>
+                </li>
+            ))}
+        </ul>
+    </div>);
+};
+
+Page.getInitialProps = async ({ req }) => {
+    const res = await fetch('http://localhost:8080/products');
+    const json = await res.json();
+    return { products: json }
+};
+
+export default Page
