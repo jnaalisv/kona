f38eec6aafb0306e2bd6a7053c6fb3a5dead9c5c
Juho Naalisvaara <juho.naalisvaara@gmail.com>
2018-10-15 08:59:55 +0300

    show error message in case of a conn error

--- frontend/pages/index.js
+++ frontend/pages/index.js
@@ -1,24 +1,36 @@
 import fetch from 'isomorphic-unfetch'
 import Link from 'next/link'
 
-const Page = ({ products }) => {
+const Page = ({ products, errorMessage }) => {
     return (
-    <div>
-        <ul>
-            {products.map(product => (
-                <li key={product.id}>
-                    <Link as={`/products/${product.id}`} href={`/products/${product.id}`}>
-                    <a>{product.name}</a>
-                    </Link>
-                </li>
-            ))}
-        </ul>
-    </div>);
+        <div>
+            {errorMessage && <p>{errorMessage}</p>}
+
+            <ul>
+                {products.map(product => (
+                    <li key={product.id}>
+                        <Link as={`/products/${product.id}`} href={`/products/${product.id}`}>
+                        <a>{product.name}</a>
+                        </Link>
+                    </li>
+                ))}
+            </ul>
+        </div>
+    );
 };
 
 Page.getInitialProps = async ({ req }) => {
-    const res = await fetch('http://localhost:8080/products');
-    const json = await res.json();
+    console.log('request headers ', req.headers);
+
+    let response;
+    const url = 'http://localhost:8080/products';
+    try {
+        response = await fetch(url);
+    } catch (err) {
+        return { products: [], errorMessage: `request to ${url} failed, is the backend down?` }
+    }
+
+    const json = await response.json();
     return { products: json }
 };
 
