c93adb6bd00dd7e05f7906759f54babc08dd3365
Juho Naalisvaara <juho.naalisvaara@gmail.com>
2018-10-16 20:47:14 +0300

    refactor to a component with state

--- frontend/components/ProductDetails.js
+++ frontend/components/ProductDetails.js
@@ -1,17 +1,45 @@
+import React from 'react'
+
 const CurrencyAmount = ({currencyAmount}) => (<p>price: {currencyAmount.amount} {currencyAmount.currency}</p>);
 
-const ProductDetails = ({product}) => (
-    <div>
-        <h4>Product</h4>
-        <p>name: {product.name}</p>
-        <p>productCode: {product.productCode}</p>
-        <p>productType: {product.productType}</p>
-        {product.price && <CurrencyAmount currencyAmount={product.price} />}
+class ProductDetails extends React.Component {
 
-        <p>id: {product.id}</p>
-        <p>version: {product.version}</p>
-        <p>createTime: {product.createTime}</p>
-    </div>
-);
+    constructor(props) {
+        super(props);
+        this.state = { product: {} };
+        this.onChange = this.onChange.bind(this);
+    }
+
+    onChange(event) {
+        event.preventDefault();
+        const product = {...this.state.product};
+        product[event.target.name] = event.target.value;
+        this.setState({ product });
+    }
+    
+    componentDidMount() {
+        const product = this.props.product;
+        this.setState({ product });
+    }
+
+    render() {
+        const { product } = this.state;
+        return (
+            <div>
+                <h4>Product</h4>
+
+                <p>name: <input name='name' value={product.name} onChange={this.onChange}/></p>
+                <p>productCode: <input name='productCode' value={product.productCode} onChange={this.onChange}/></p>
+                <p>productType: {product.productType}</p>
+                {product.price && <CurrencyAmount currencyAmount={product.price} />}
+
+                <p>id: {product.id}</p>
+                <p>version: {product.version}</p>
+                <p>createTime: {product.createTime}</p>
+                <button>Save</button>
+            </div>
+        )
+    }
+}
 
 export default ProductDetails;
