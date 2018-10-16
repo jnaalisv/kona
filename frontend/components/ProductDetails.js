00f5edda5e6f6232290abd770fe9b004df196827
Juho Naalisvaara <juho.naalisvaara@gmail.com>
2018-10-16 14:01:22 +0300

    reorder

--- frontend/components/ProductDetails.js
+++ frontend/components/ProductDetails.js
@@ -3,13 +3,14 @@ const CurrencyAmount = ({currencyAmount}) => (<p>price: {currencyAmount.amount} 
 const ProductDetails = ({product}) => (
     <div>
         <h4>Product</h4>
-        <p>id: {product.id}</p>
         <p>name: {product.name}</p>
         <p>productCode: {product.productCode}</p>
         <p>productType: {product.productType}</p>
+        {product.price && <CurrencyAmount currencyAmount={product.price} />}
+
+        <p>id: {product.id}</p>
         <p>version: {product.version}</p>
         <p>createTime: {product.createTime}</p>
-        {product.price && <CurrencyAmount currencyAmount={product.price} />}
     </div>
 );
 
