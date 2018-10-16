2e550c20223bf58c57823c43ad6367afb3395bf6
Juho Naalisvaara <juho.naalisvaara@gmail.com>
2018-10-16 12:45:07 +0300

    refactor into components

--- frontend/pages/product.js
+++ frontend/pages/product.js
@@ -2,19 +2,27 @@ import { withRouter } from 'next/router'
 import Layout from '../components/Layout'
 import { getProductById } from '../services/ProductService';
 
+const CurrencyAmount = ({currencyAmount}) => (<p>price: {currencyAmount.amount} {currencyAmount.currency}</p>);
+
+const ProductDetails = ({product}) => (
+    <div>
+        <h4>Product</h4>
+        <p>id: {product.id}</p>
+        <p>name: {product.name}</p>
+        <p>productCode: {product.productCode}</p>
+        <p>productType: {product.productType}</p>
+        <p>version: {product.version}</p>
+        <p>createTime: {product.createTime}</p>
+        {product.price && <CurrencyAmount currencyAmount={product.price} />}
+    </div>
+);
+
 const ProductPage = withRouter(({product, errorMessage, router, url}) => {
     return (
         <Layout>
             <p>Location: {router.asPath}</p>
             {errorMessage && <p>{errorMessage}</p>}
-            <h4>Product</h4>
-            <p>id: {product.id}</p>
-            <p>name: {product.name}</p>
-            <p>productCode: {product.productCode}</p>
-            <p>productType: {product.productType}</p>
-            <p>version: {product.version}</p>
-            <p>createTime: {product.createTime}</p>
-            {product.price && <p>price: {product.price.amount} {product.price.currency}</p>}
+            <ProductDetails product={product} />
         </Layout>
     )
 });
