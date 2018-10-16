c276729e0907acc771891641e7c6a1843adb8d1e
Juho Naalisvaara <juho.naalisvaara@gmail.com>
2018-10-16 13:42:03 +0300

    extract Product-components

---
+++ frontend/components/ProductListing.js
@@ -0,0 +1,22 @@
+import Link from 'next/link'
+
+const ProductLink = props => (
+    <li>
+        <Link href={`/products?id=${props.id}`}>
+            <a>{props.name}</a>
+        </Link>
+    </li>
+);
+
+const ProductListing = ({products}) => (
+    <div>
+        <h4>Product listing</h4>
+        <ul>
+            {products.map(product => (
+                <ProductLink key={product.id} name={product.name} id={product.id}/>
+            ))}
+        </ul>
+    </div>
+);
+
+export default ProductListing;
