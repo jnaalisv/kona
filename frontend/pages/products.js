662d18d65387bf5d07a0102a6bc11ba1aa6b85f2
Juho Naalisvaara <juho.naalisvaara@gmail.com>
2018-10-17 12:29:18 +0300

    refactor products-page

--- frontend/pages/products.js
+++ frontend/pages/products.js
@@ -1,15 +1,33 @@
 import Layout from '../components/Layout'
 import ProductListing from '../components/ProductListing'
 import { getAllProducts } from '../services/ProductService';
+import React from 'react'
 
-const ProductListingPage = ({ products, errorMessage }) => (
-    <Layout error={errorMessage}>
-        <ProductListing products={products} />
-    </Layout>
-);
+class ProductListingPage extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            products: []
+        }
+    }
 
-ProductListingPage.getInitialProps = async props => {
-    return await getAllProducts();
-};
+    componentDidMount() {
+        getAllProducts()
+            .then(response => {
+                this.setState({
+                    products: response.products,
+                    errorMessage: response.errorMessage
+                })
+            });
+    }
+
+    render() {
+        return (
+            <Layout error={this.state.errorMessage}>
+                <ProductListing products={this.state.products} />
+            </Layout>
+        );
+    }
+}
 
 export default ProductListingPage
