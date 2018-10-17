a6f54624fdbdd1e51e465d61a733ba6d1dbfde2d
Juho Naalisvaara <juho.naalisvaara@gmail.com>
2018-10-17 21:33:19 +0300

    move all product state handling to product.js

--- frontend/pages/product.js
+++ frontend/pages/product.js
@@ -1,18 +1,48 @@
+import React from 'react'
+import { withRouter } from 'next/router'
 import Layout from '../components/Layout'
-import ProductDetails from '../components/ProductDetails'
+import ProductForm from '../components/ProductForm'
 import { getProductById } from '../services/ProductService';
 
-const ProductPage = ({product, errorMessage}) => (
-    <Layout error={errorMessage}>
-        <ProductDetails product={product} />
-    </Layout>
-);
+class ProductPage extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            product: {
+                name: '',
+                productCode: ''
+            },
+            errorMessage: ''
+        };
 
-ProductPage.getInitialProps = async props => {
-    return await getProductById(props.query.id);
-};
+        this.handleChange = this.handleChange.bind(this);
+    }
 
-export default ProductPage
+    componentDidMount() {
+        getProductById(this.props.router.query.id)
+            .then(response => {
+                this.setState({
+                    product: response.product,
+                    errorMessage: response.errorMessage
+                });
+            });
+    }
 
+    handleChange(event) {
+        event.preventDefault();
+        const product = {...this.state.product};
+        product[event.target.name] = event.target.value;
+        this.setState({ product });
+    }
 
+    render() {
+        const { product, errorMessage } = this.state;
+        return (
+            <Layout error={errorMessage}>
+                <ProductForm product={product} handleChange={this.handleChange}/>
+            </Layout>
+        );
+    }
+}
 
+export default withRouter(ProductPage)
