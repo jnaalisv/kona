ee8853d90b4b02297d75f23737cdd61567970c83
Juho Naalisvaara <juho.naalisvaara@gmail.com>
2018-10-18 12:07:51 +0300

    refactor

--- frontend/services/ProductService.js
+++ frontend/services/ProductService.js
@@ -1,4 +1,4 @@
-import fetch from 'isomorphic-unfetch'
+import { httpGET, httpPUT, httpPOST } from './http'
 const productsUrl = 'http://localhost:8080/products';
 
 export const getProductById = productId =>
@@ -13,41 +13,4 @@ export const saveOrUpdateProduct = product => {
     } else {
         return httpPOST(productsUrl, product);
     }
-};
-
-
-const defaultOptions = {
-    // mode: 'cors',
-    headers: {
-        'Content-Type': 'application/json;charset=UTF-8',
-//        'Accept': 'application/json;charset=UTF-8',
-    }
-};
-
-export const httpGET = url => doHttp('GET', url);
-export const httpPOST = (url, body) => doHttp('POST', url, body);
-export const httpPUT = (url, body) => doHttp('PUT', url, body);
-
-const handleResponse = response => {
-    if (response.status < 300) {
-
-        const contentTypeHeader = response.headers.get('content-type');
-
-        if (contentTypeHeader && contentTypeHeader.toLowerCase().startsWith('application/json')) {
-            return response.json();
-        }
-        return response.text();
-    }
-    return Promise.reject(new Error(response));
-};
-
-const doHttp = (method, url, body) => {
-    const options = {...defaultOptions};
-    options.method = method;
-
-    if (body) {
-        options.body = JSON.stringify(body);
-    }
-
-    return fetch(url, options).then(handleResponse)
 };
