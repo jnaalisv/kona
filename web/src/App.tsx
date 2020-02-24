1d40f04c846dd68fca9a747092ab5dddf313f064
Juho Naalisvaara <juho.naalisvaara@gmail.com>
2020-02-24 22:57:00 +0200

    add react-router-dom and simple routing

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,13 +1,41 @@
 import React from 'react';
 import './App.css';
+import {
+    BrowserRouter as Router,
+    Switch,
+    Route,
+    Link
+} from "react-router-dom";
+
 import ProductTable from "./components/ProductTable";
 
 const App = () => {
-  return (
-    <div className="App">
-        <ProductTable />
-    </div>
-  );
+    return (
+        <Router>
+            <div>
+                <nav>
+                    <ul>
+                        <li>
+                            <Link to="/">Home</Link>
+                        </li>
+                        <li>
+                            <Link to="/products">products</Link>
+                        </li>
+                    </ul>
+                </nav>
+                <Switch>
+                    <Route path="/products">
+                        <ProductTable />
+                    </Route>
+                    <Route path="/">
+                        <Home />
+                    </Route>
+                </Switch>
+            </div>
+        </Router>
+    );
 };
 
 export default App;
+
+const Home = () => (<h2>Home</h2>);
