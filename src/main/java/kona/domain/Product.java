1e1cfb6db5b2d5d2dd7689ab784fdade19678d32
Juho Naalisvaara <juho.naalisvaara@gmail.com>
2020-03-29 14:15:32 +0300

    use Java14 "records" preview feature

--- src/main/java/kona/domain/Product.java
+++ src/main/java/kona/domain/Product.java
@@ -6,62 +6,12 @@ import java.math.BigDecimal;
 import java.time.LocalDateTime;
 
 @JsonAutoDetect(fieldVisibility = JsonAutoDetect.Visibility.ANY)
-public class Product {
-
-    private long id;
-    private long version;
-    private LocalDateTime created;
-    private String name;
-    private String code;
-    private ProductType productType;
-    private BigDecimal price;
-
-    @java.beans.ConstructorProperties({"name", "code", "productType", "price"})
-    public Product(String name, String code, ProductType productType, BigDecimal price) {
-        this.name = name;
-        this.code = code;
-        this.productType = productType;
-        this.price = price;
-        this.version =1L;
-        this.created = LocalDateTime.now();
-    }
-
-    @java.beans.ConstructorProperties({"id", "version", "created", "name", "code", "productType", "price"})
-    public Product(long id, long version, LocalDateTime created, String name, String code, ProductType productType, BigDecimal price) {
-        this.id = id;
-        this.version = version;
-        this.created = created;
-        this.name = name;
-        this.code = code;
-        this.productType = productType;
-        this.price = price;
-    }
-
-    public String name() {
-        return name;
-    }
-
-    public long id() {
-        return this.id;
-    }
-
-    public ProductType productType() {
-        return productType;
-    }
-
-    public BigDecimal price() {
-        return price;
-    }
-
-    public long version() {
-        return version;
-    }
-
-    public LocalDateTime created() {
-        return created;
-    }
-
-    public String code() {
-        return code;
-    }
-}
+public record Product(
+        long id
+        , long version
+        , LocalDateTime created
+        , String name
+        , String code
+        , ProductType productType
+        , BigDecimal price
+) { }
