7e24029976ba806c7301b8f0204864003ad025f3
Juho Naalisvaara <juho.naalisvaara@gmail.com>
2020-04-10 20:50:28 +0300

    add test case

--- src/test/java/kona/http/SerializationTest.java
+++ src/test/java/kona/http/SerializationTest.java
@@ -3,6 +3,7 @@ package kona.http;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import kona.domain.Product;
+import kona.domain.ProductType;
 import kona.persistance.InMemProductRepository;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -26,5 +27,27 @@ class SerializationTest {
         assertThat(json).containsSequence("\"name\"");
         assertThat(json).containsSequence("\"code\"");
         assertThat(json).containsSequence("\"productType\"");
+    }
+
+    @Test
+    void deserializationToObjectSucceeds() throws JsonProcessingException {
+        String json = """
+                {
+                    "code": "CCU",
+                    "created": "2020-04-10T20:43:45.591918",
+                    "id": 2,
+                    "name": "Central Computing Unit",
+                    "price": 280.0,
+                    "productType": "EQUIPMENT",
+                    "version": 0
+                }
+                """;
+
+        Product product = this.objectMapper.readValue(json, Product.class);
+
+        assertThat(product.code()).isEqualTo("CCU");
+        assertThat(product.productType()).isEqualTo(ProductType.EQUIPMENT);
+        assertThat(product.name()).isEqualTo("Central Computing Unit");
+        assertThat(product.id()).isEqualTo(2);
     }
 }
